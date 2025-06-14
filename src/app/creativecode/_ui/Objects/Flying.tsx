89bad75cbc9773355fcf36f7ce76d351891f3cbe
Lok Lok <yellowhappy831@gmail.com>
2025-06-14 20:45:27 +0800

    save

--- src/app/creativecode/_ui/Objects/Flying.tsx
+++ src/app/creativecode/_ui/Objects/Flying.tsx
@@ -13,15 +13,12 @@ export function Flying() {
     st.gl.render(st.scene, st.camera)
   }, 1)
 
-  const uni = useMemo(() => {
-    const color1 = uniform(new THREE.Color('#ff0000'))
-    const color2 = uniform(new THREE.Color('#0000ff'))
-
+  const Unis = useMemo(() => {
     return {
       //
       size: uniform(1.0),
-      color1: color1,
-      color2: color2,
+      color1: uniform(new THREE.Color('#ff0000')),
+      color2: uniform(new THREE.Color('#0000ff')),
 
       frequencyX: uniform(10),
       frequencyY: uniform(10),
@@ -35,14 +32,14 @@ export function Flying() {
 
     const modelPosition = vec4(positionLocal, 1)
 
-    const xBand = sin(modelPosition.x.mul(uni.frequencyX).sub(time)).mul(0.1)
-    const yBand = sin(modelPosition.y.mul(uni.frequencyY).sub(time)).mul(0.1)
-    const elevation = xBand.add(yBand).mul(uni.size)
+    const xBand = sin(modelPosition.x.mul(Unis.frequencyX).sub(time)).mul(0.1)
+    const yBand = sin(modelPosition.y.mul(Unis.frequencyY).sub(time)).mul(0.1)
+    const elevation = xBand.add(yBand).mul(Unis.size)
 
     material.positionNode = positionLocal.add(vec3(0, 0, elevation))
     material.normalNode = positionLocal.add(vec3(0, 0, elevation)).normalize()
 
-    material.colorNode = mix(uni.color1, uni.color2, sin(elevation).mul(cos(elevation)).mul(0.5).add(0.5))
+    material.colorNode = mix(Unis.color1, Unis.color2, sin(elevation).mul(cos(elevation)).mul(0.5).add(0.5))
 
     return { material }
   }, [])
@@ -52,9 +49,10 @@ export function Flying() {
     let box1 = FlyPlaneSheet.object(
       'flying1',
       {
-        position: { x: 0, y: 0, z: 0 },
-        rotation: { x: 0, y: 0, z: 0 },
-        scale: { x: 1, y: 1, z: 1 },
+        // position: { x: 0, y: 0, z: 0 },
+        // rotation: { x: 0, y: 0, z: 0 },
+        // scale: { x: 1, y: 1, z: 1 },
+        //
         colorA: types.rgba({ ...new THREE.Color('#ff0000'), a: 1 }, {}),
         colorB: types.rgba({ ...new THREE.Color('#0000ff'), a: 1 }, {}),
         frequencyX: 10,
@@ -71,34 +69,24 @@ export function Flying() {
       },
     )
 
+    //
+
     return box1.onValuesChange((values) => {
       //
 
-      uni.frequencyX.value = values.frequencyX
-      uni.frequencyY.value = values.frequencyY
+      Unis.frequencyX.value = values.frequencyX
+      Unis.frequencyY.value = values.frequencyY
 
-      uni.color1.value.setRGB(values.colorA.r, values.colorA.g, values.colorA.b)
-      uni.color2.value.setRGB(values.colorB.r, values.colorB.g, values.colorB.b)
+      Unis.color1.value.setRGB(values.colorA.r, values.colorA.g, values.colorA.b)
+      Unis.color2.value.setRGB(values.colorB.r, values.colorB.g, values.colorB.b)
 
-      uni.size.value = values.size
+      Unis.size.value = values.size
 
       material.metalness = values.metalness
 
       //
     })
-  }, [uni])
-
-  // useControls({
-  //   frequency: {
-  //     value: [20, 20],
-  //     onChange: (value: [number, number]) => {
-  //       uni.frequencyX.value = value[0]
-  //       uni.frequencyY.value = value[1]
-  //     },
-  //   },
-  // })
-
-  //
+  }, [Unis])
 
   return (
     <>
