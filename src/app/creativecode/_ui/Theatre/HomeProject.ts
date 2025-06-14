261d6374df14bc2d458a17bc93577cea60faf05c
Lok Lok <yellowhappy831@gmail.com>
2025-06-14 19:45:54 +0800

    save

--- src/app/creativecode/_ui/Theatre/HomeProject.ts
+++ src/app/creativecode/_ui/Theatre/HomeProject.ts
@@ -1,3 +1,12 @@
 import { getProject } from '@theatre/core'
+// import statev001 from './'
+import state001 from './HomeProject/HomeProject.theatre-project-state.json'
+let config: any = {
+  state: false,
+}
 
-export const HomeProject = getProject('HomeProject')
+if (process.env.NODE_ENV !== 'development') {
+  config.state = state001
+}
+
+export const HomeProject = getProject('HomeProject', config)
