bde421f8ef97cf0ef23815e408362fb4730a6554
Lok Lok <yellowhappy831@gmail.com>
2025-06-17 21:28:30 +0800

    save

--- src/app/blue/page.tsx
+++ src/app/blue/page.tsx
@@ -10,52 +10,29 @@ export default function Page() {
   let router = useRouter()
   return (
     <>
-      <div className='w-full h-full'>
-        <O3D>
-          <Suspense fallback={null}>
-            <Environment files={[`/hdr/brown_photostudio_02_1k.hdr`]} background></Environment>
-
-            <Flying />
-            <OrbitControls object-position={[0, 1.5, 2.5]}></OrbitControls>
-
-            <group>
-              <Box
-                onClick={() => {
-                  //
-                  router.push('/creativecode')
-                }}
-              >
-                <meshBasicMaterial color={'blue'}></meshBasicMaterial>
-              </Box>
-            </group>
-          </Suspense>
-        </O3D>
-        {/* <Suspense fallback={null}>
-          <Environment files={[`/hdr/brown_photostudio_02_1k.hdr`]}></Environment>
+      <O3D>
+        <Suspense fallback={null}>
+          <Environment files={[`/hdr/brown_photostudio_02_1k.hdr`]} background></Environment>
 
           <Flying />
-
           <OrbitControls object-position={[0, 1.5, 2.5]}></OrbitControls>
-        </Suspense> */}
-        {/*  */}
-        {/* <Canvas
-        gl={async (st: any) => {
-          const renderer = new WebGPURenderer({
-            canvas: st.canvas as HTMLCanvasElement,
-          })
-          renderer.shadowMap.enabled = true
-          renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-          if (!renderer._initialized) {
-            await renderer.init()
-          }
+          <group>
+            <Box
+              onClick={() => {
+                //
+                router.push('/creativecode')
+              }}
+            >
+              <meshBasicMaterial color={'blue'}></meshBasicMaterial>
+            </Box>
+          </group>
+        </Suspense>
+      </O3D>
 
-          return renderer
-        }}
-      >
-       
-      </Canvas> */}
-      </div>
+      <div className='w-full h-full'></div>
     </>
   )
 }
+
+//
